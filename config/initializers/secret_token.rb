3e9165a2b53817a1cf336e6485771eea9ace40bc
Andrew Cranston <andrewtcranston@gmail.com>
2014-02-22 18:42:01 -0800

    new rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shipping::Application.config.secret_key_base = '766dfa1d91c39a192e9aea6edd9e153c02bd9fd56bae277f315b1971d161c7f1c0aeefce28eb3a008507e7591b583e3d3ab16660b165ca0390a900aa071524dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shipping::Application.config.session_store :cookie_store, key: '_shipping_session'
